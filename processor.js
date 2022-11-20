614319776a84a810a7bcaf6d56671bedefdf8c1d
carrel <dvquentincarrel@gmail.com>
2022-11-20 23:55:19 +0100

    TP4 Presque final

--- processor.js
+++ processor.js
@@ -1,23 +1,25 @@
+
+
 function make_nav(data, current){
-	var nav = document.getElementsByTagName("nav")[0]
+	let nav = document.getElementsByTagName("nav")[0]
 	nav.innerHTML = ""
 	for(i = 0; i < data.length; i++){
-		div = document.createElement("div")
-		link = document.createElement("a")
+		let div = document.createElement("div")
+		let link = document.createElement("a")
 		//link.href = data[i].link
 		link.href = "#"
-		link.setAttribute("onclick", "inject_page(pages, '"+data[i].name+"', '"+data[i].text+"')")
+		link.setAttribute("onclick", "inject_page(pages_json, '"+data[i].name+"', '"+data[i].text+"')")
 		link.innerHTML = data[i].text
 		if(current == data[i].text){
 			link.id = "current_page"
 		}
 		div.appendChild(link)
 		if(data[i].submenu){
-			ul = document.createElement("ul")
+			let ul = document.createElement("ul")
 			ul.classList.add("drop_down")
 			for(j = 0; j < data[i].submenu.length; j++){
-				li = document.createElement("li")
-				sublink = document.createElement("a")
+				let li = document.createElement("li")
+				let sublink = document.createElement("a")
 				sublink.href = (data[i].submenu[j].link) ? data[i].submenu[j].link : "#"
 				sublink.innerHTML = data[i].submenu[j].text
 				li.appendChild(sublink)
@@ -29,111 +31,90 @@ function make_nav(data, current){
 	}
 }
 
-function make_table(data){
-	// Does what it says, parsing data, a dict of lists describing the content of the table
-	table = document.getElementsByTagName("table")[0]
+function make_player_widgets(){
+	for(i = 0; i < joueurs_data.length; i++){
+		let container = document.createElement("div")
 
-	// Header
-	tr = document.createElement("tr")
-	for(i = 0; i < data['th'].length; i++){
-		td = document.createElement("th")
-		td.innerHTML = data['th'][i]
-		tr.appendChild(td)
+		let name = document.createElement("p")
+		name.innerHTML = joueurs_data[i][0]
+		container.appendChild(name)
+
+		let desc = document.createElement("p")
+		desc.innerHTML = joueurs_data[i][3]
+		container.appendChild(desc)
+
+		let img = document.createElement("img")
+		img.setAttribute('src', joueurs_data[i][1])
+		container.appendChild(img)
+
+		let poste = document.createElement("p")
+		poste.innerHTML = joueurs_data[i][2]
+		container.appendChild(poste)
+
+		let btns = document.createElement("div")
+		let btn_1 = document.createElement('button')
+		btn_1.innerHTML = 'Modifier'
+		btn_1.setAttribute('onclick', 'mod_player(this);update_widgets()')
+		btns.appendChild(btn_1)
+		let btn_2 = document.createElement('button')
+		btn_2.innerHTML = 'Supprimer'
+		btn_2.setAttribute('onclick', 'del_player(this);update_widgets()')
+		btns.appendChild(btn_2)
+		container.appendChild(btns)
+
+		let id = document.createElement("p")
+		id.innerHTML = joueurs_data[i][4]
+		id.style.display = "none"
+		container.appendChild(id)
+
+		document.getElementsByTagName('table')[0].appendChild(container)
 	}
-	td = document.createElement("th")
-	tr.appendChild(td)
-	table.appendChild(tr)
-
-	// Rows
-	for(i = 0; i < data['td'].length; i++){
-		add_row(data['td'][i], table)
-	}
 }
 
-function add_row(row, table){
-	tr = document.createElement("tr")
-	for(j = 0; j < row.length; j++){
-		td = document.createElement("td")
-		if(j == 1){
-			// If it's the picture
-			img = document.createElement("img")
-			img.src = row[j]
-			td.appendChild(img)
-		}else{
-			td.innerHTML = row[j]
-		}
-		tr.appendChild(td)
-	}
-	td = document.createElement("td")
-	btn = document.createElement("button")
-	btn.innerHTML = "Agrandir"
-	btn_del = document.createElement("button")
-	btn_del.innerHTML = "Supprimer"
-	btn_del.setAttribute('onclick' ,"this.parentElement.parentElement.remove()")
-	btn_mod = document.createElement("button")
-	btn_mod.innerHTML = "Modifier"
-	btn_mod.setAttribute('onclick' ,"modify_row(this)")
-	td.appendChild(btn)
-	td.appendChild(document.createElement("br"))
-	td.appendChild(btn_del)
-	td.appendChild(document.createElement("br"))
-	td.appendChild(btn_mod)
-	td.appendChild(document.createElement("br"))
-	tr.appendChild(td)
-	table.appendChild(tr)
+function update_widgets(){
+	document.getElementsByTagName('table')[0].innerHTML = ''
+	make_player_widgets()
 }
 
-function get_fields_data(){
-	let nom = document.getElementById("nm")
-	let photo = document.getElementById("photo")
-	let poste = document.getElementById("pst")
-	let description = document.getElementById("dsc")
-	let data = [nom.value, photo.value, poste.value, description.value]
-	empty_fields()
-	return data
+function mod_player(elem){
+	let row_id = elem.parentNode.parentNode.lastChild.innerHTML
+	let row_data = joueurs_data.filter((val, index, arr) => {
+		return val[4] == row_id
+	})
+	
+	row_data[0] = document.querySelector('#nm').value 
+	row_data[1] = document.querySelector('#photo').value 
+	row_data[2] = document.querySelector('#pst').value 
+	row_data[3] = document.querySelector('#dsc').value
 }
 
-function empty_fields(){
-	let nom = document.getElementById("nm").value = ""
-	let photo = document.getElementById("photo").value = ""
-	let poste = document.getElementById("pst").value = ""
-	let description = document.getElementById("dsc").value = ""
+function del_player(elem){
+	let row_id = elem.parentNode.parentNode.lastChild.innerHTML
+	joueurs_data = joueurs_data.filter((val, index, arr) => {
+		return val[4] != row_id
+	})
 }
 
-function new_row(){
-	table = document.getElementsByTagName("table")[0]
-	data = get_fields_data()
-	for(i = 0; i < data.length; i++){
-		// if not all fields are filled
-		if(data[i] == ""){
-			alert("Tous les champs ne sont pas remplis")
-			return 1;
-		}
-	}
-	add_row(data, table)
-}
-
-function modify_row(elem){
-	console.log(elem)
-	let description = elem.parentElement.previousElementSibling
-	console.log(description)
-	let poste = description.previousElementSibling
-	let photo = poste.previousElementSibling
-	let nom = photo.previousElementSibling
-	let data = get_fields_data()
-	console.log(data)
-	description.innerHTML = data[3]
-	poste.innerHTML = data[2]
-	photo.innerHTML = data[1]
-	nom.innerHTML = data[0]
-	console.log(description)
+function add_player_widget(){
+	let new_data = []
+	let name = document.querySelector('#nm').value 
+	new_data.push(name)
+	let photo = document.querySelector('#photo').value 
+	new_data.push(photo)
+	let pst = document.querySelector('#pst').value 
+	new_data.push(pst)
+	let dsc = document.querySelector('#dsc').value 
+	new_data.push(dsc)
+	joueurs_data.push(new_data)
 }
 
 function inject_page(pages, page, nav_name){
-	make_nav(nav, nav_name)
+	// Loads a page content into the current web page
+	make_nav(nav_json, nav_name)
 	let anchor = document.getElementById("anchor")
-	anchor.innerHTML = pages[page]
-	if(page == 'joueurs'){
-		make_table(joueurs)
+	let sel_page = pages[page]
+	anchor.innerHTML = sel_page['content']
+	if(sel_page['callback'] != ""){
+		eval(sel_page['callback'])
 	}
 }
